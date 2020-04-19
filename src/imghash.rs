655604ec75d3c0227a30521c1514cefec5e53f01
Felix Giese <felix.giese@icloud.com>
2020-04-19 18:48:56 +0200

    make it compile quite a while later

--- src/imghash.rs
+++ src/imghash.rs
@@ -1,21 +1,104 @@
-
-
 use image;
 use image::imageops;
+use std::fmt::Display;
 
-pub fn hash_img(filename : &String) -> image::ImageResult<u64> {
-    image::open(filename).map(|i| {
-        let i = imageops::grayscale(&i.resize_exact(8,8,image::FilterType::Lanczos3));;
-        let sum : u16 = i.pixels().map(|p| p[0] as u16).sum();
-        let avg = sum / 64;
+pub struct Hash {
+    backing: Vec<u64>,
+    size: u16,
+}
 
-        let mut hash : u64 = 0;
-        i.pixels().for_each(|p| {
-            hash <<= 1;
-            if p[0] as u16 >= avg {
-                hash ^= 1;
+impl Display for Hash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut nibbles = self.size as i32 / 4;
+        let mut index = 0;
+        while nibbles > 0 {
+            let width = usize::min(nibbles as usize, 16);
+            f.write_fmt(format_args!(
+                "{:0width$x}",
+                self.backing[index],
+                width = width,
+            ))?;
+            nibbles -= 16;
+            index += 1;
+        }
+        Ok(())
+    }
+}
+
+struct HashBuilder {
+    backing: Vec<u64>,
+    target_size: u16,
+    size: u16,
+}
+
+pub enum HashError {
+    NotEnoughBits,
+    DecoderError(image::ImageError),
+}
+
+impl From<image::ImageError> for HashError {
+    fn from(from: image::ImageError) -> Self {
+        HashError::DecoderError(from)
+    }
+}
+
+impl Display for HashError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            HashError::NotEnoughBits => f.write_fmt(format_args!("Not enough bits in the buffer")),
+            HashError::DecoderError(inner) => f.write_fmt(format_args!("Decoder Error: {}", inner)),
+        }
+    }
+}
+
+impl HashBuilder {
+    fn new(size: u16) -> HashBuilder {
+        let full_words = size / 64;
+        let extra_word_needed = size % 64 > 0;
+        let word_count = full_words + if extra_word_needed { 1 } else { 0 };
+        HashBuilder {
+            backing: Vec::with_capacity(word_count as usize),
+            target_size: size,
+            size: 0,
+        }
+    }
+    fn add_bit(&mut self, is_one: bool) {
+        if self.size % 64 == 0 {
+            self.backing.push(if is_one { 1 } else { 0 });
+        } else {
+            let hash_part = self
+                .backing
+                .last_mut()
+                .expect("This cannot happen because the size would be zero otherwise");
+            *hash_part <<= 1;
+            if is_one {
+                *hash_part ^= 1;
             }
-        });
-        hash
-    })
+        }
+        self.size += 1;
+    }
+
+    fn finalize(self) -> Result<Hash, HashError> {
+        if self.size < self.target_size {
+            return Err(HashError::NotEnoughBits);
+        }
+        Ok(Hash {
+            backing: self.backing,
+            size: self.target_size,
+        })
+    }
+}
+
+pub fn hash_img(filename: &str, hash_size: u16) -> Result<Hash, HashError> {
+    let img = image::open(filename)?;
+    let img = imageops::grayscale(&img.resize_exact(8, 8, image::FilterType::Triangle));
+
+    let sum: u64 = img.pixels().map(|p| p[0] as u64).sum();
+    let avg = sum / (hash_size as u64);
+
+    let mut hash = HashBuilder::new(hash_size);
+    for p in img.pixels() {
+        hash.add_bit(p[0] as u64 >= avg);
+    }
+    hash.finalize()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 extern crate image;
 extern crate rayon;
-extern crate itertools;
 
-use itertools::Itertools;
 use rayon::prelude::*;
 
 mod imghash;
 
-use std::io::Lines;
 use std::env;
-use std::io;
-use std::io::BufReader;
-use std::io::BufRead;
 use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Lines;
 
 #[derive(Debug)]
 enum Inputfile {
     Stdin,
-    Filename(String)
+    Filename(String),
 }
 
 enum InputfileIterator {
     StdinBase(Lines<BufReader<io::Stdin>>),
-    FileBase(Lines<BufReader<File>>)
+    FileBase(Lines<BufReader<File>>),
 }
 
 impl Iterator for InputfileIterator {
@@ -30,26 +28,24 @@ impl Iterator for InputfileIterator {
     fn next(&mut self) -> Option<Self::Item> {
         match self {
             InputfileIterator::StdinBase(b) => b.next().map(|s| s.unwrap()),
-            InputfileIterator::FileBase(b) => b.next().map(|s| s.unwrap())
+            InputfileIterator::FileBase(b) => b.next().map(|s| s.unwrap()),
         }
     }
 }
 
 impl Inputfile {
-    fn new(name : String) -> Inputfile {
+    fn new(name: String) -> Inputfile {
         match name.as_ref() {
-            "-" => {
-                Inputfile::Stdin
-            }
-            _ => {
-                Inputfile::Filename(name)
-            }
+            "-" => Inputfile::Stdin,
+            _ => Inputfile::Filename(name),
         }
     }
     fn lines(&self) -> InputfileIterator {
         match self {
             Inputfile::Stdin => InputfileIterator::StdinBase(BufReader::new(io::stdin()).lines()),
-            Inputfile::Filename(f) => InputfileIterator::FileBase(BufReader::new(File::open(f).unwrap()).lines())
+            Inputfile::Filename(f) => {
+                InputfileIterator::FileBase(BufReader::new(File::open(f).unwrap()).lines())
+            }
         }
     }
 }
@@ -61,36 +57,58 @@ fn main() {
     let mut batch_files = Vec::new();
     env::args().skip(1).for_each(|arg| {
         match arg.as_ref() {
-            "-b" => { batchmode = true; }
+            "-b" => {
+                batchmode = true;
+            }
             _ if batchmode => {
-                    batch_files.push(Inputfile::new(arg));
-                }
+                batch_files.push(Inputfile::new(arg));
+            }
             _ => {
-                    raw_files.push(arg);
-                }
+                raw_files.push(arg);
+            }
         };
     });
 
-    raw_files.into_iter().chain( batch_files.into_iter().flat_map(|f| f.lines()))
-    .chunks(1000)
-    .into_iter()
-    .map(|a| a.collect::<Vec<_>>())
-    .for_each(|a| a.par_iter()
-        .map(|f| (imghash::hash_img(&f),f))
-        .for_each(|(h,f)| {
-            match h {
-                Ok(h) => println!("{:016x}\t{}", h, f),
-                Err(e) => eprintln!("Error: {} in File {}", e, f)
-            };
-        })
-    );
-        // .for_each(|f| {
-        //     pool.spawn(move || {
-        //         let h = imghash::hash_img(&f);
-        //         match h {
-        //             Ok(h) => println!("{:016x}\t{}", h, f),
-        //             Err(e) => eprintln!("Error: {} in File {}", e, f)
-        //         };
-        //     });
-        // });
+    rayon::ThreadPoolBuilder::new().build_global().unwrap();
+
+    let all_files = raw_files
+        .into_iter()
+        .chain(batch_files.into_iter().flat_map(|f| f.lines()));
+
+    all_files.par_bridge().for_each(|f| {
+        let hash = imghash::hash_img(&f, 64);
+        match hash {
+            Ok(h) => {
+                println!("{}\t->\t{}", f, h);
+            }
+            Err(e) => {
+                println!("{}\t->\t{}", f, e);
+            }
+        }
+    });
+    // all_files.par_iter().for_each(|f| {
+    //     println!("f");
+    // });
+    // .chunks(1000)
+    // .into_iter()
+    // .map(|a| a.collect::<Vec<_>>())
+    // .for_each(|a| {
+    //     a.par_iter()
+    //         .map(|f| (imghash::hash_img(&f), f))
+    //         .for_each(|(h, f)| {
+    //             match h {
+    //                 Ok(h) => println!("{}\t{}", h.to_string(), f),
+    //                 Err(e) => eprintln!("Error: {} in File {}", e, f),
+    //             };
+    //         })
+    // });
+    // .for_each(|f| {
+    //     pool.spawn(move || {
+    //         let h = imghash::hash_img(&f);
+    //         match h {
+    //             Ok(h) => println!("{:016x}\t{}", h, f),
+    //             Err(e) => eprintln!("Error: {} in File {}", e, f)
+    //         };
+    //     });
+    // });
 }
